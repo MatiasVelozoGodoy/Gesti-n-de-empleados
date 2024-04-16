fdaca943a61dcb5b738334fb91e99b854fb74b13
Matias Velozo <matias.velozo.4c@gmail.com>
2024-04-16 02:12:21 -0300

    creando las clases personas y profesiones

---
+++ Dominio/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public int DNI { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos{ get; set; }
+        public int Edad { get; set; }
+        public string Descripcion { get; set; }
+        public Profesiones Profesion { get; set; }
+    }
+}
