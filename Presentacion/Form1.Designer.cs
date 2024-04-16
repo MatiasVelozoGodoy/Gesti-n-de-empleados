0f5047e03e1f91dcfa0f999d5b85386c586bec3c
Matias Velozo <matias.velozo.4c@gmail.com>
2024-04-16 01:35:13 -0300

    agregando imagen formulario principal y editando el readme

--- Presentacion/Form1.Designer.cs
+++ Presentacion/Form1.Designer.cs
@@ -48,12 +48,15 @@
             this.txtBuscar = new System.Windows.Forms.TextBox();
             this.btnMinizar = new System.Windows.Forms.Button();
             this.btnCerrar = new System.Windows.Forms.Button();
+            this.pbxIMGinicial = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panelMenu.SuspendLayout();
             this.panelSubReportes.SuspendLayout();
             this.panelSubGestion.SuspendLayout();
             this.panelFoto.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbxIMG)).BeginInit();
             this.panelControl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxIMGinicial)).BeginInit();
             this.SuspendLayout();
             // 
             // panelMenu
@@ -393,6 +396,27 @@
             this.btnCerrar.UseVisualStyleBackColor = true;
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
+            // pbxIMGinicial
+            // 
+            this.pbxIMGinicial.Image = ((System.Drawing.Image)(resources.GetObject("pbxIMGinicial.Image")));
+            this.pbxIMGinicial.Location = new System.Drawing.Point(412, 157);
+            this.pbxIMGinicial.Name = "pbxIMGinicial";
+            this.pbxIMGinicial.Size = new System.Drawing.Size(248, 223);
+            this.pbxIMGinicial.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxIMGinicial.TabIndex = 2;
+            this.pbxIMGinicial.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 47.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(177)))), ((int)(((byte)(176)))));
+            this.label1.Location = new System.Drawing.Point(370, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 65);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "¡Bienvenido!";
+            // 
             // Empresa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -400,10 +424,13 @@
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(77)))), ((int)(((byte)(96)))));
             this.ClientSize = new System.Drawing.Size(922, 670);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbxIMGinicial);
             this.Controls.Add(this.panelControl);
             this.Controls.Add(this.panelMenu);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Empresa";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gestion";
@@ -414,7 +441,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbxIMG)).EndInit();
             this.panelControl.ResumeLayout(false);
             this.panelControl.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxIMGinicial)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -439,6 +468,8 @@
         private System.Windows.Forms.Button btnProfesion;
         private System.Windows.Forms.Button btnDesempleados;
         private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.PictureBox pbxIMGinicial;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Gesti-n-de-empleados
-Una aplicación de escritorio para administrar perfiles 
+# Gestión-de-empleados
+Una aplicación de escritorio para administrar perfiles
+[![1.png](https://i.postimg.cc/nLMc134V/1.png)](https://postimg.cc/mcfsLNpK)
+[![2.png](https://i.postimg.cc/Xq6j43TX/2.png)](https://postimg.cc/WFSL7RpP)
+[![3.png](https://i.postimg.cc/4yvJ3qgP/3.png)](https://postimg.cc/bSJXC6dS)
